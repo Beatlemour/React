73b687fbeb4617405069307998d73ce7557143d9
unknown <aashish.maharjan.mails@gmail.com>
2020-09-02 20:59:59 +1000

    if conditional rendering

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
 
 export class UserGreeting extends Component {
-    
+
     constructor(props) {
         super(props)
-    
+
         this.state = {
-             isLoggedIn : true
+            isLoggedIn: true
         }
     }
     render() {
-        if(this.state.isLoggedIn){
+        return this.state.isLoggedIn && <div>Hello Aashish</div>
+
+        //return this.state.isLoggedIn ? <div>Welcome Aashish</div> : <div>Welcome Guest</div>
+
+        /* if(this.state.isLoggedIn){
             return (
                 <div>
                     Welcome Aashish
@@ -21,7 +25,7 @@ export class UserGreeting extends Component {
             <div>
                 Please Login
             </div>
-        )}
+        )} */
     }
 }
 
